a88bf5020b68da6c1d5737556bef79f2f05b03c8
Priyank-vibin <vibinpriya91@gmail.com>
2022-06-29 12:38:44 +0530

    this is the first commit

---
+++ cal.js
@@ -0,0 +1,23 @@
+//1.display number intext box
+function displayNum(num){
+    result.value+=num
+}
+
+//2 . clear text box
+function clearBox(){
+    result.value=""
+}
+
+//3.evaluate expression
+function evaluateExpr(){
+result.value = eval((result.value))
+}
+ 
+//4.remove last item from text box
+function removeItem(){
+    curExpr = result.value
+    result.value =curExpr.slice(0,-1)
+}
+
+
+
